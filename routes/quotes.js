7afb4aa25090eaebbed263175401706cb31abb54
Maximilian Alexander <maxalex@zinkpulse.com>
2013-11-14 21:55:28 -0500

    removing old files

--- routes/quotes.js
+++
@@ -1,9 +0,0 @@
-var stockService = require('../services/stockService')
-
-exports.list = function(req, res){
-    stockService.getQuotes(req.param('ticker'), req.query.start, req.query.end).then(function(data){
-        res.send(data);
-    }, function(error){
-        res.send(error);
-    });
-};
