25ad8dd50cc9a224d1a11bba51e86d12e1d6cf79
Maximilian Alexander <max@kensho.com>
2013-11-15 12:57:51 -0500

    Adding Menu Items    (use "git reset HEAD <file>..." to unstage)

--- js/app.js
+++ js/app.js
@@ -1,22 +1,4 @@
 
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ngRoute']);
 
 
-app.controller('homeController', function($scope){
-
-	$scope.people = [
-		{
-			name: "Max",
-			age: 23
-		},
-		{
-			name: "Mary",
-			age: 90
-		},
-		{
-			name: "John",
-			age: 12
-		}
-	];
-
-});
